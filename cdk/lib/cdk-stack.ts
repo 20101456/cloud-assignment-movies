a6b9b234020ff9b79a5be7780864dbf1cd119075
20101456 <116279250+20101456@users.noreply.github.com>
2026-08-05 14:23:25 +0100

    Added API Gateway and Lambda infrastructure

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,12 +1,14 @@
 import * as cdk from 'aws-cdk-lib/core';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
 import { Construct } from 'constructs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // DynamoDB Table - Single table design
     const moviesTable = new dynamodb.Table(this, 'MoviesTable', {
       partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
@@ -14,10 +16,113 @@ export class CdkStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    // Output
+    const commonLambdaProps = {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'index.handler',
+      environment: {
+        MOVIES_TABLE: moviesTable.tableName,
+      },
+    };
+
+    const getMovieRolesFunction = new lambda.Function(
+      this,
+      'GetMovieRolesFunction',
+      {
+        ...commonLambdaProps,
+        code: lambda.Code.fromAsset(
+          path.join(__dirname, '../../lambdas/get-movie-roles')
+        ),
+      }
+    );
+
+    const getActorFunction = new lambda.Function(
+      this,
+      'GetActorFunction',
+      {
+        ...commonLambdaProps,
+        code: lambda.Code.fromAsset(
+          path.join(__dirname, '../../lambdas/get-actor')
+        ),
+      }
+    );
+
+    const postRoleFunction = new lambda.Function(
+      this,
+      'PostRoleFunction',
+      {
+        ...commonLambdaProps,
+        code: lambda.Code.fromAsset(
+          path.join(__dirname, '../../lambdas/post-role')
+        ),
+      }
+    );
+
+    const deleteRoleFunction = new lambda.Function(
+      this,
+      'DeleteRoleFunction',
+      {
+        ...commonLambdaProps,
+        code: lambda.Code.fromAsset(
+          path.join(__dirname, '../../lambdas/delete-role')
+        ),
+      }
+    );
+
+    moviesTable.grantReadData(getMovieRolesFunction);
+    moviesTable.grantReadData(getActorFunction);
+    moviesTable.grantWriteData(postRoleFunction);
+    moviesTable.grantWriteData(deleteRoleFunction);
+
+    const api = new apigateway.RestApi(this, 'MoviesApi', {
+      restApiName: 'Movies API',
+      description: 'Serverless API for managing movie cast roles',
+      deployOptions: {
+        stageName: 'prod',
+      },
+    });
+
+    const movies = api.root.addResource('movies');
+
+    const movieRoles = movies
+      .addResource('{movieID}')
+      .addResource('roles');
+
+    movieRoles.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(getMovieRolesFunction)
+    );
+
+    movieRoles
+      .addResource('{actorID}')
+      .addMethod(
+        'DELETE',
+        new apigateway.LambdaIntegration(deleteRoleFunction)
+      );
+
+    movies
+      .addResource('roles')
+      .addMethod(
+        'POST',
+        new apigateway.LambdaIntegration(postRoleFunction)
+      );
+
+    const actors = api.root.addResource('actors');
+
+    actors
+      .addResource('{actorID}')
+      .addMethod(
+        'GET',
+        new apigateway.LambdaIntegration(getActorFunction)
+      );
+
     new cdk.CfnOutput(this, 'TableName', {
       value: moviesTable.tableName,
       description: 'DynamoDB table name',
+    });
+
+    new cdk.CfnOutput(this, 'ApiUrl', {
+      value: api.url,
+      description: 'Movies API URL',
     });
   }
 }
